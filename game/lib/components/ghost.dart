851aaabf028eab6945638d3a61dc896a928fceca
Luan Nico <luanpotter27@gmail.com>
2021-10-29 18:25:54 -0400

    Acc movement

--- game/lib/components/ghost.dart
+++ game/lib/components/ghost.dart
@@ -1,14 +1,19 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
 
 class Ghost extends PositionComponent {
   static const double _radius = 30.0;
-  static const double _speed = 300.0;
+  static const double _maxSpeed = 350.0;
+  static const double _acc = 250.0;
+
   static final Paint _paint = Paint()..color = const Color(0xFFFF0000);
 
   // current direction being pressed by the player
   final Vector2 move = Vector2.zero();
+
+  Vector2 speed = Vector2.zero();
 
   Ghost() {
     anchor = Anchor.center;
@@ -19,7 +24,11 @@ class Ghost extends PositionComponent {
   void update(double dt) {
     super.update(dt);
 
-    position += move * _speed * dt;
+    final targetSpeed = move.clone()..scaleTo(_maxSpeed);
+    final acc = (targetSpeed - speed)..scaleTo(_acc);
+
+    speed += acc * dt;
+    position += speed * dt + acc * dt * dt / 2;
   }
 
   @override
