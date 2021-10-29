2716ef6537018a6f14096c5e08686bfdcd985899
Erick Zanardo <erickzanardoo@gmail.com>
2021-10-29 18:54:20 -0300

    adding enemy movement

--- game/lib/components/enemy.dart
+++ game/lib/components/enemy.dart
@@ -31,21 +31,46 @@ class Enemy extends PositionComponent with HasGameRef<GhostGame> {
 
   Enemy(this.type);
 
+  final random = Random();
+
   final EnemyType type;
+
+  late EnemyData data;
+  late double target;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
-    final data = enemies[type]!;
+    data = enemies[type]!;
 
     size = data.size;
 
-    final random = Random();
 
     final area = gameRef.size.x - size.x;
     position.x = area * random.nextDouble();
     position.y = gameRef.size.y - size.y;
+
+    _chooseTarget();
+  }
+
+  void _chooseTarget() {
+    final area = gameRef.size.x - size.x;
+    target = area * random.nextDouble();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    final dir = x < target
+        ? 1 : -1;
+
+    position.x += data.speed * dir * dt;
+
+    if ((position.x - target).abs() <= 5) {
+      _chooseTarget();
+    }
   }
 
   @override
