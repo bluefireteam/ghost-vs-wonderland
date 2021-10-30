0b58da0f2ae5836b12f8028daa2a0019db273457
Luan Nico <luanpotter27@gmail.com>
2021-10-29 21:06:27 -0400

    Gravity

--- game/lib/components/ghost.dart
+++ game/lib/components/ghost.dart
@@ -1,13 +1,22 @@
+import 'dart:ui';
+
 import 'package:flame/components.dart';
 import 'package:flame/extensions.dart';
-import 'package:ghost_vs_wonderland/game.dart';
 
+import '../game.dart';
 import 'minion.dart';
 
 class Ghost extends SpriteComponent with HasGameRef<GhostGame> {
+  static final Paint _auraPaint = Paint()
+    ..color = const Color(0x88FFFFFF)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 2.0;
+
   static const double _size = 400.0;
-  static const double _maxSpeed = 350.0;
-  static const double _acc = 250.0;
+  static const double _maxSpeed = 450.0;
+  static const double _acc = 350.0;
+  static const double _auraStrength = 450.0;
+  static const double _auraRadius = 120.0;
 
   // current direction being pressed by the player
   final Vector2 move = Vector2.zero();
@@ -26,12 +35,22 @@ class Ghost extends SpriteComponent with HasGameRef<GhostGame> {
 
     sprite = await gameRef.loadSprite('hero.png');
 
-    minionSpawn = Timer(4, callback: _addMinion, repeat: true)
-        ..start();
+    minionSpawn = Timer(4, callback: _addMinion, repeat: true)..start();
   }
 
   void _addMinion() {
     gameRef.add(Minion());
+  }
+
+  Vector2 computeEffect(Vector2 element) {
+    final distance = element - position;
+    final l = distance.length;
+    if (l > _auraRadius || l == 0) {
+      return Vector2.zero();
+    }
+
+    final d = l / _auraRadius;
+    return distance..scaleTo(_auraStrength / (d * d));
   }
 
   @override
@@ -44,5 +63,11 @@ class Ghost extends SpriteComponent with HasGameRef<GhostGame> {
 
     speed += acc * dt;
     position += speed * dt + acc * dt * dt / 2;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawCircle((size / 2).toOffset(), _auraRadius, _auraPaint);
   }
 }
